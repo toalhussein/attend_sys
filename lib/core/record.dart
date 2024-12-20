d0921cab5e2053721d427320d36c471cd755c10b
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-12-20 16:41:29 +0200

    fixing pubspec bugs

--- lib/core/record.dart
+++ lib/core/record.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:attend_sys/core/app_snackbars.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
